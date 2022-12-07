161e447adbd94be858b96e0d70db7fbf109b8939
Jorge Sanes <jsanes@condorlabs.io>
2022-12-07 09:31:36 -0500

    Add new bump_type output

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const core = require('@actions/core');
 
-const commitMessage = core.getInput('commit_message')
-const delimiter = 'JIRA tickets:';
+const commitMessage = core.getInput('commit_message');
 
-if (!commitMessage.includes(delimiter)) {
+const messageDelimiter = '---';
+const jiraDelimiter = 'JIRA tickets:';
+const versionDelimiter = 'New version bump:'
+
+if (!commitMessage.includes(versionDelimiter)) {
+  throw new Error('No delimiter found in the commit message. Add the `---` delimiter to the commit message.');
+}
+
+if (!commitMessage.includes(jiraDelimiter)) {
   throw new Error('No delimiter found in the commit message. Add the `JIRA tickets:` delimiter to the commit message.');
 }
 
-const [description, links] = commitMessage.split(delimiter)
+if (!commitMessage.includes(versionDelimiter)) {
+  throw new Error('No delimiter found in the commit message. Add the `New version bump:` delimiter to the commit message.');
+}
 
-const message = description.trim()
-const linksList = links && links.trim().split(',');
+const [message, rest] = commitMessage.trim().split(messageDelimiter);
+const [links, version] = rest.split(versionDelimiter);
+const linksList = links.trim().split(jiraDelimiter)[1].split(',');
 
-core.setOutput('message', message);
-core.setOutput('links_list', linksList)
+const newMessage = message.trim();
+const newLinksList = linksList.map(link => link.trim());
+const newVersionBump = version.trim();
+
+core.setOutput('message', newMessage);
+core.setOutput('links_list', newLinksList);
+core.setOutput('bump_type', newVersionBump);
